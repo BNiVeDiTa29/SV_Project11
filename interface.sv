fafa1d1dbaea9c168a7fd2b67478087c1a74bf2e
BNiVeDiTa29 <148596813+BNiVeDiTa29@users.noreply.github.com>
2024-03-20 21:49:51 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,9 +1,4 @@
 interface Intel8088Pins(input bit CLK, RESET);
-
-
-logic [19:0] Address;
-wire [7:0]  Data, Dbus;
-
 logic MNMX = '1;
 logic TEST = '1;
 
@@ -25,42 +20,13 @@ logic ALE;
 logic DTR;
 logic DEN;
 
-logic CS,CS1,CS2,CS3,CS4;
+logic [19:0] Address;
+wire [7:0] Data;
 
 
-//fsm
-
-// 8282 Latch to latch bus address
-always_latch
-begin
-if (ALE)
-	Address <= {A, AD};
-end
-
-// 8286 transceiver
-assign Data =  (DTR & ~DEN) ? AD   : 'z;
-assign AD   = (~DTR & ~DEN) ? Data : 'z;
-
-
-//selction logic
-always_comb
-	begin
-	{CS1,CS2,CS3,CS4} = 4'b0000;
-
-	if (!Address[19] && !IOM)
-		CS1 = 1;
-	else if (Address[19] && !IOM)
-		CS2 = 1;
-	else if (IOM && Address[15:4] == 12'hFF0)
-		CS3 = 1;
-	else if (IOM && Address[15:9] == 7'h0E)
-		CS4 = 1;
-	end
 
 modport Processor(input CLK, RESET, HOLD, READY, NMI, INTR, MNMX, TEST, inout AD, output A, output HLDA, output IOM, output WR, output RD, output SSO, output INTA, output ALE, output DTR, output DEN);
 
-modport Pheripheral0(input CLK, RESET, ALE, IOM, CS1, RD, WR, Address, inout Data);
-modport Pheripheral1(input CLK, RESET, ALE, IOM, CS2, RD, WR, Address, inout Data);
-modport Pheripheral2(input CLK, RESET, ALE, IOM, CS3, RD, WR, Address, inout Data);
-modport Pheripheral3(input CLK, RESET, ALE, IOM, CS4, RD, WR, Address, inout Data);
+modport Pheripheral(input CLK, RESET, ALE, IOM, RD, WR, Address, inout Data);
+
 endinterface
