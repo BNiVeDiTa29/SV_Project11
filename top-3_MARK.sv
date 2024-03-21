4494940ca018d906b6eb0b5e5f16aaeeb2a77116
BNiVeDiTa29 <148596813+BNiVeDiTa29@users.noreply.github.com>
2024-03-20 21:56:35 -0700

    Update top-3_MARK.sv

--- top-3_MARK.sv
+++ top-3_MARK.sv
@@ -3,22 +3,41 @@ module top;
 bit CLK = '0;
 bit RESET = '0;
 
-//Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
+logic [3:0] CS;
+//modules instantiation
 Intel8088Pins i (.*);
 Intel8088 P (.i(i.Processor));
-IOM_Module0 M0 (i.Pheripheral0);
-IOM_Module1 M1 (i.Pheripheral1);
-IOM_Module2 I0 (i.Pheripheral2);
-IOM_Module3 I1 (i.Pheripheral3);
-/*
-IOM_Module M1(CLK, RESET, ALE, IOM, CS1, RD, WR, Address, Data);
-IOM_Module M2(CLK, RESET, ALE, IOM, CS2, RD, WR, Address, Data);
-IOM_Module M3(CLK, RESET, ALE, IOM, CS3, RD, WR, Address, Data);
-IOM_Module M4(CLK, RESET, ALE, IOM, CS4, RD, WR, Address, Data);
-*/
+IOM_Module #(0, 20'h7ffff, 20'h80000, 20'hfffff, 16'hff00, 16'hff0f, 16'h1c00, 16'h1dff) m0(i.Pheripheral, CS);
+IOM_Module #(0, 20'h7ffff, 20'h80000, 20'hfffff, 16'hff00, 16'hff0f, 16'h1c00, 16'h1dff) m1(i.Pheripheral, CS);
+IOM_Module #(1, 20'h7ffff, 20'h80000, 20'hfffff, 16'hff00, 16'hff0f, 16'h1c00, 16'h1dff) io0(i.Pheripheral, CS);
+IOM_Module #(1, 20'h7ffff, 20'h80000, 20'hfffff, 16'hff00, 16'hff0f, 16'h1c00, 16'h1dff) io1(i.Pheripheral, CS);
 
 
+always_latch
+begin
+if (i.ALE)
+	i.Address <= {i.A, i.AD};
+end
 
+// 8286 transceiver
+assign i.Data =  (i.DTR & ~i.DEN) ? i.AD   : 'z;
+assign i.AD   = (~i.DTR & ~i.DEN) ? i.Data : 'z;
+
+
+//chip selction logic
+always_comb
+	begin
+	CS= 4'b0000;
+
+	if (!i.Address[19] && !i.IOM)
+		CS[0] = 1'd1;
+	else if (i.Address[19] && !i.IOM)
+		CS[1] = 1'd1;
+	else if (i.IOM && i.Address[15:4] == 12'hFF0)
+		CS[2] = 1'd1;
+	else if (i.IOM && i.Address[15:9] == 7'h0E)
+		CS[3] = 1'd1;
+	end
 
 always #50 CLK = ~CLK;
 
@@ -34,7 +53,5 @@ RESET = '0;
 repeat(10000) @(posedge CLK);
 $finish();
 end
-
-//Chip Select logic
 
 endmodule
